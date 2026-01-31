f603a4d36147e1ac7829fa2167943f17e895048d
thatdevnerd <bas@bitsenbytes.org>
2026-01-31 15:11:21 +0100

    init

---
+++ src/main/java/org/playerjoin/Kit/JoinKit.java
@@ -0,0 +1,32 @@
+package org.playerjoin.Kit;
+
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class JoinKit {
+    private final Player player;
+    private final ItemStack[] items = new ItemStack[0];
+    private final HashMap<String, Integer> defaultStack = new HashMap<>(Map.of(
+            "Weapon_Sword_Crude", 1,
+            "Tool_Pickaxe_Crude", 1,
+            "Tool_Hatchet_Crude", 1,
+            "Food_Wildmeat_Cooked", 12
+    ));
+
+    public JoinKit(Player player) {
+        this.player = player;
+    }
+
+    public void grant() {
+        AtomicReference<Short> slot = new AtomicReference<>((short) 0);
+        defaultStack.forEach((item, amount) -> {
+            ItemStack stack = new ItemStack(item, amount);
+            this.player.getInventory().getCombinedHotbarFirst()
+                    .setItemStackForSlot(slot.getAndSet((short) (slot.get() + 1)), stack);
+        });
+    }
+}
---
+++ src/main/java/org/playerjoin/Messenger.java
@@ -0,0 +1,83 @@
+package org.playerjoin;
+
+import com.hypixel.hytale.component.Component;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.event.EventRegistry;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.event.events.player.AddPlayerToWorldEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+import org.playerjoin.Kit.JoinKit;
+
+import java.awt.*;
+import java.util.logging.Level;
+
+
+public class Messenger extends JavaPlugin {
+    private HytaleLogger logger = HytaleLogger.forEnclosingClass();
+
+    public Messenger(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    public void setup() {
+        EventRegistry eventRegistry = getEventRegistry();
+        eventRegistry.registerGlobal(PlayerReadyEvent.class, this::onPlayerJoin);
+        eventRegistry.registerGlobal(AddPlayerToWorldEvent.class, this::onPlayerJoinWorld);
+    }
+
+    @Override
+    public void start() {
+        logger.at(Level.INFO).log("Booting up PlayerJoinMessenger");
+    }
+
+    @Override
+    public void shutdown() {
+        logger.at(Level.INFO).log("Shutting down PlayerJoinMessenger");
+    }
+
+    public void onPlayerJoinWorld(AddPlayerToWorldEvent event) {
+        event.setBroadcastJoinMessage(false);
+    }
+
+    public void onPlayerJoin(PlayerReadyEvent event) {
+        Player eventPlayer = event.getPlayer();
+        if (eventPlayer.isFirstSpawn()) {
+            Universe.get().getPlayers().forEach(player -> {
+                Player p = refToPlayerComponent(player);
+                if (p != eventPlayer) {
+                    player.sendMessage(Message.raw("Player {player} joined for the first time"
+                            .replace("{player}", event.getPlayer().getDisplayName())));
+                }
+            });
+            eventPlayer.sendMessage(Message.raw("Welcome to hytaleworld!").color(Color.CYAN));
+            new JoinKit(eventPlayer).grant();
+        } else {
+            Universe.get().getPlayers().forEach(player -> {
+                Player p = refToPlayerComponent(player);
+                if (p != eventPlayer) {
+                    player.sendMessage(Message.raw("Player {player} logged in"
+                            .replace("{player}", event.getPlayer().getDisplayName())));
+                }
+            });
+            eventPlayer.sendMessage(Message.raw("Welcome back to hytale world").color(Color.gray));
+        }
+    }
+
+    public Player refToPlayerComponent(PlayerRef p) {
+        Ref<EntityStore> pRef = p.getReference();
+        Store<EntityStore> store = pRef.getStore();
+        return store.getComponent(pRef, Player.getComponentType());
+    }
+}
